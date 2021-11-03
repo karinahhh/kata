3848f110218a5f32bd30da047596e51ac0899ea8
opilane <opilane@HP-CZC6297B94.tthk.ee>
2021-11-03 13:51:13 +0200

    req4 zero start pass

--- src/IPvalidator.java
+++ src/IPvalidator.java
@@ -4,7 +4,7 @@ import java.util.function.BooleanSupplier;
 public class IPvalidator {
 
 	public boolean ValidateIp4Address(String ipString) {
-		if(ipString=="") {
+		if(hasThreeDots(ipString) && firstNumberInRange1_254(ipString)) {
 			return false;
 		}
 		return true;
@@ -18,5 +18,9 @@ public class IPvalidator {
 
 	public int[] getNumbers(String string) {
 		return Arrays.stream(string.split("\\.")).mapToInt(Integer::parseInt).toArray();
+	}
+	
+	private boolean firstNumberInRange1_254(String ipString) {
+		return getNumbers(ipString)[0]>0 && getNumbers(ipString)[0]<255;
 	}
 }
